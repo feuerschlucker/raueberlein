14055692ecd905bde726eeb5aa280e7daec55274
feuerschlucker <hroethl@gmail.com>
2024-04-14 13:51:55 +0200

    bound

--- pack2/Aufteilen3.java
+++ pack2/Aufteilen3.java
@@ -9,7 +9,7 @@ import com.gurobi.gurobi.GRBLinExpr;
 import com.gurobi.gurobi.GRBModel;
 import com.gurobi.gurobi.GRBVar;
 
-public class Aufteilen2 {
+public class Aufteilen3 {
 
 	// Neues Kommentar
 	private GRBEnv env;
@@ -19,7 +19,7 @@ public class Aufteilen2 {
 	private static int numberOfLPsSolvedUsingGurobi;
 	private double startbound;
 
-	public Aufteilen2(ArrayList<Item> items) throws GRBException {
+	public Aufteilen3(ArrayList<Item> items) throws GRBException {
 		this.items = items;
 		this.no_items = items.size();
 		this.werte = new double[no_items];
@@ -76,47 +76,69 @@ public class Aufteilen2 {
 				System.out.print(x[i].get(GRB.StringAttr.VarName) + " = " + x[i].get(GRB.DoubleAttr.X) + ", ");
 			}
 			System.out.println("");
-
-			for (int i = 0; i < no_items; i++) {
-				// System.out.println("i= "+i+" "+x[i].get(GRB.DoubleAttr.X)+ "
-				// "+model.get(GRB.DoubleParam.IntFeasTol));
-				if (x[i].get(GRB.DoubleAttr.X) > model.get(GRB.DoubleParam.IntFeasTol)
-						&& x[i].get(GRB.DoubleAttr.X) < 1 - model.get(GRB.DoubleParam.IntFeasTol))
-
+			
+			for (int i = 0; i < no_items; i++)
+			{
+				//System.out.println("i= "+i+" "+x[i].get(GRB.DoubleAttr.X)+ "  "+model.get(GRB.DoubleParam.IntFeasTol));
+				if (x[i].get(GRB.DoubleAttr.X) > model.get(GRB.DoubleParam.IntFeasTol) && x[i].get(GRB.DoubleAttr.X) < 1 - model.get(GRB.DoubleParam.IntFeasTol))
+					
 				{
 					fixedTo0[i] = true;
+					double[] checkbound = new double[no_items];
+					for (int j = 0;j<no_items;j++) {
+						if (j != i) {
+							checkbound[j] = x[j].get(GRB.DoubleAttr.X);
+						}else{
+							checkbound[j] =0;
+						};
+					}
+					double[] solution0;
+					if (Math.abs(valueFunction(checkbound))< startbound) {
+						startbound = Math.abs(valueFunction(checkbound));
+						solution0 = branchAndBound(fixedTo0, fixedTo1);
+						fixedTo0[i] = false;
+					}else{
+						solution0 = null;
+					}
 
-					double[] solution0 = branchAndBound(fixedTo0, fixedTo1);
-					fixedTo0[i] = false;
 
 					fixedTo1[i] = true;
 					double[] solution1 = branchAndBound(fixedTo0, fixedTo1);
 					fixedTo1[i] = false;
-
-					if (solution0 == null) {
+					
+					if (solution0 == null)
+					{
 						model.dispose();
 						return solution1;
-					} else if (solution1 == null) {
+					}
+					else if (solution1 == null)
+					{
 						model.dispose();
 						return solution0;
-					} else {
+					}
+					else
+					{
 						double ov0 = valueFunction(solution0);
 						double ov1 = valueFunction(solution1);
-						if (Math.abs(ov0) < Math.abs(ov1)) {
+						if (Math.abs(ov0) < Math.abs(ov1))
+						{
 							model.dispose();
 							return solution0;
-						} else {
+						}
+						else
+						{
 							model.dispose();
 							return solution1;
 						}
 					}
 				}
 			}
-
-			for (int i = 0; i < no_items; i++) {
-				result[i] = x[i].get(GRB.DoubleAttr.X);
+			
+			for (int i = 0; i < no_items; i++)
+			{			
+				result[i]=  x[i].get(GRB.DoubleAttr.X);	
 			}
-
+			
 		} else {
 			System.out.println("No optimum solution found.");
 		}
@@ -132,19 +154,20 @@ public class Aufteilen2 {
 		}
 		return vf;
 	}
-
+	
+	
 	public double startHeuristic(ArrayList<Item> items) {
 		double sum1 = 0.0;
-		double sum2 = 0.0;
-		for (int i = 0; i < items.size(); i++) {
-			if (items.get(i).getWert() + sum1 >= sum2) {
+		double sum2 =0.0;
+		for (int i = 0; i<items.size();i++) {
+			if(items.get(i).getWert()+sum1 >=sum2) {
 				sum2 += items.get(i).getWert();
-			} else {
-				sum1 += items.get(i).getWert();
-			}
+			}else {
+				sum1+=items.get(i).getWert();
+			}	
 		}
-
-		return Math.abs(sum2 - sum1);
+		
+		return Math.abs(sum2-sum1);
 	}
 
 	public double xxxBound(ArrayList<Item> items) {
@@ -164,15 +187,15 @@ public class Aufteilen2 {
 			System.out.println(item.getWert() + "   " + item.getBezeichnung());
 		}
 
-		Aufteilen2 auft = new Aufteilen2(items);
+		Aufteilen3 auft = new Aufteilen3(items);
 		double startbound = auft.startHeuristic(items);
-
+		
 		auft.modelSetup(startbound);
 
 		System.out.println(startbound);
-
+		
 		double[] solution = auft.branchAndBound(new boolean[no_items], new boolean[no_items]);
-
+		
 		System.out.println(solution);
 
 		System.out.println("optimum solution (Differenz = " + auft.valueFunction((solution)) + "):");
